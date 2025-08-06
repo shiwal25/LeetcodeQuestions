6166250ad83b50e52a150e86d999e40c93867f96
Shivam Agrawal <154428392+shiwal25@users.noreply.github.com>
2025-08-06 19:09:49 +0530

    Time: 0 ms (100%), Space: 45.3 MB (55.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int x = 0;
+        for(int i=0;i<nums.length;i++){
+            x = x^i^nums[i];
+        }
+        x = x^nums.length;
+        return x;
+    }
+}
