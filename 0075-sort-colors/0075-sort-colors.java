80dbef690cac49eca349a6e569232bf29d70d63b
Shivam Agrawal <154428392+shiwal25@users.noreply.github.com>
2025-08-06 18:55:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.91%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+    }
+    void mergeSort(int nums[],int l,int r){
+        if(l<r){
+            int m = l+(r-l)/2;
+            mergeSort(nums,l,m);
+            mergeSort(nums,m+1,r);
+            merge(nums,l,m,r);
+        }
+    }
+    void merge(int nums[],int l, int m, int r){
+        int n1 = m-l+1;
+        int n2 = r-m;
+        int arr1[] = new int[n1];
+        int arr2[] = new int[n2];
+        for(int i=0;i<n1;i++){
+            arr1[i] = nums[l+i];
+        }
+        for(int i=0;i<n2;i++){
+            arr2[i] = nums[m+i+1];
+        }
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while(i<n1 && j<n2){
+            if(arr1[i]<=arr2[j]){
+                nums[k] = arr1[i];
+                i++;
+                k++;
+            }
+            else{
+                nums[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<n1){
+            nums[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            nums[k] = arr2[j];
+            j++;
+            k++;
+        }
+    }
+}
