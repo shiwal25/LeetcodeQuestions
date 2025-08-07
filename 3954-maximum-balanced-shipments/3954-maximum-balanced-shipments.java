a4d76b9a5bb45e380e04675b8053ef86e0c3cfa9
Shivam Agrawal <154428392+shiwal25@users.noreply.github.com>
2025-08-07 07:27:46 +0530

    Time: 3 ms (61.03%), Space: 62.4 MB (57.02%) - LeetHub

--- 3954-maximum-balanced-shipments/3954-maximum-balanced-shipments.java
+++ 3954-maximum-balanced-shipments/3954-maximum-balanced-shipments.java
@@ -1,26 +1,36 @@
 class Solution {
     public int maxBalancedShipments(int[] weight) {
+        // int res = 0;
+        // int i=0;
+        // int j = 0;
+        // while(i<weight.length-1){
+        //     int mw = weight[i];
+        //     j = i+1;
+        //     boolean found = false;
+        //     while(j<weight.length && weight[j]<mw){
+        //         mw = Math.max(mw,weight[j-1]);
+        //         if(mw>weight[j]){
+        //             res++;
+        //             i = j+1;
+        //             found = true;
+        //             break;
+        //         }
+        //         j++;
+        //     }
+        //     if(!found){
+        //         i++;
+        //     }
+        // }
         int res = 0;
-        int i=0;
-        int j = 0;
-        while(i<weight.length-1){
-            int mw = weight[i];
-            j = i+1;
-            boolean found = false;
-            while(j<weight.length && weight[j]<mw){
-                mw = Math.max(mw,weight[j-1]);
-                if(mw>weight[j]){
-                    res++;
-                    i = j+1;
-                    found = true;
-                    break;
-                }
-                j++;
-            }
-            if(!found){
+        int mw = 0;
+        for(int i=0;i<weight.length-1;i++){
+            mw = Math.max(mw,weight[i]);
+            if(weight[i+1]<mw){
+                mw = 0;
+                res++;
                 i++;
             }
         }
-    return res;
+        return res;
     }
 }
