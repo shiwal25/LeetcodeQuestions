1f84389b3a5ef81f6c44ce54f8ead8e29cc97c02
Shivam Agrawal <154428392+shiwal25@users.noreply.github.com>
2025-08-26 00:15:08 +0530

    Time: 0 ms (100%), Space: 40.8 MB (75.86%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i=0;i<32;i++){
+            res = res<<1;
+            res = res|((n>>i)&1);
+        }
+        return res;
+    }
+}
