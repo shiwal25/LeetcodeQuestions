27cb259efa90fe9688f818b108116a204340c9b4
Shivam Agrawal <154428392+shiwal25@users.noreply.github.com>
2025-08-25 23:50:21 +0530

    Time: 7 ms (16.87%), Space: 46.4 MB (93.65%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int res[] = new int[n+1];
         for(int i=0;i<=n;i++){
             int count = 0;
-            for(int j=1;j<=32;j++){
+            for(int j=0;j<32;j++){
                 if( ((i>>j)&1) == 1){
                     count ++;
                 }
